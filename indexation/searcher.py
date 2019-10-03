cc22997630e605014497ad7789017e76079821cd
Pierre POMERET-COQUOT <pierre.pomeret@irit.fr>
2019-10-03 15:21:50 +0200

    Add naive reordering using ups + samples queries test

--- indexation/searcher.py
+++ indexation/searcher.py
@@ -10,8 +10,7 @@ from org.apache.lucene.search.similarities import \
      TFIDFSimilarity, LMDirichletSimilarity, BM25Similarity
 
 
-
-
+N_DOCS = 10
 
 def run(searcher, analyzer):
     while True:
@@ -24,12 +23,33 @@ def run(searcher, analyzer):
         print()
         print("Searching for:", command)
         query = QueryParser("body", analyzer).parse(command)
-        scoreDocs = searcher.search(query, 50).scoreDocs
+        scoreDocs = searcher.search(query, N_DOCS).scoreDocs
         print("%s total matching documents." % len(scoreDocs))
 
-        for scoreDoc in scoreDocs:
+
+        # Reordering using ups
+        scores = {}
+        n_docs = len(scoreDocs)
+        for i, scoreDoc in enumerate(scoreDocs):
             doc = searcher.doc(scoreDoc.doc)
-            print(doc.get('name'), doc.get('body')[:100], '...')
+            scores[scoreDoc.doc] = (n_docs-i, int(doc.get('ups')))
+            ## TODO: normalize 'ups'
+            ## (divide it by the maximum 'ups' of the subreddit)
+            ## + weight scores
+
+        print(scores)
+
+        scoreDocs = sorted(scoreDocs, key=lambda sd: -sum(scores[sd.doc]))
+
+        # Print results
+        for i, scoreDoc in enumerate(scoreDocs):
+            doc = searcher.doc(scoreDoc.doc)
+            print()
+            print("%d (sc=%d+%d=%d) (%s)"%(
+                i,
+                *scores[scoreDoc.doc], sum(scores[scoreDoc.doc]),
+                doc.get('name')))
+            print(doc.get('body'))
 
         
 
@@ -62,6 +82,7 @@ if __name__ == "__main__":
     # Sample query
     storeDir = SimpleFSDirectory(Paths.get(args.index_dir))
     searcher = IndexSearcher(DirectoryReader.open(storeDir))
-    searcher.setSimilarity(similarity)
+    if similarity is not None:
+        searcher.setSimilarity(similarity)
     analyzer = StandardAnalyzer()
     run(searcher, analyzer)
