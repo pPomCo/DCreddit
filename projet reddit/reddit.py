617a8fa7f5cf43ac1ecd9d34b91c4dcade534133
douceurnocturne <xzaitsevx@gmail.com>
2019-10-03 13:49:08 +0200

    Add files via upload
    part 2 clean data

---
+++ projet reddit/reddit.py
@@ -0,0 +1,55 @@
+#partie 2 The Reddit Project
+
+import numpy as np
+import sklearn as sk
+import pandas as pd
+
+#Recupération des donnees
+#Récupérons les labels 
+#Clean data
+
+#E: Des données 
+#F: Retourne les données préparés
+#S: Des données
+#Procedure Source :
+#https://towardsdatascience.com/the-art-of-cleaning-your-data-b713dbd49726
+def clean_data(df):
+	# Missing data (generalement ont les enlèves):
+	df.dropna(axis=0, how='any')
+	# Outliers (ont regarde les 100 premiers et derniers):
+	lower_limit = np.percentille(df.ups,0,0002)
+	upper_limit = np.percentille(df.ups,0,00098)
+	print(lower_limit)
+	print()
+	print(upper_limit)
+		#potentiel transfo de x en enlevant ces 100 valeurs ovnies
+	# Irrevelant features (Obliger score + down):
+	df.drop('downs', axis=1)
+	df.drop('score_hidden', axis=1)
+	return df
+	
+#E: Des données, une liste de features
+#F: Enlève les features de la liste a nos données
+#S: Des données
+def rmv_features(df,features):
+	for i in range(len(features)):
+		df.drop(features[i], axis=1)
+	return df
+	
+#E: Des données, un tableau de features
+#F:	Regroupe les données par thème
+#S: Des liste de données
+def regroup(df,tab):
+	feature_groups = {}
+	data=[]
+	for i in range(len(tab)):
+		feature_group[i]=', '.join(tab[i])
+		data.append(df[tab[i]])
+	return (data,feature_group)
+	
+
+# Au cas ou split en train / test / validation
+# Train 70 Test 20% Validation 10%
+x_train, x_b, y_train, y_b = train_test_split(x, y, train_size=0.80, random_state=42)
+x_test, x_ validation, y_test, y_validation(x_b, y_b, train_size=0.66 random_state=42)
+
