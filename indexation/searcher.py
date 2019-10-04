fffd7e6b9e64377857a306cd6bc9a04f001bf433
Pierre POMERET-COQUOT <pierre.pomeret@irit.fr>
2019-10-04 08:50:04 +0200

    Refactoring (reordering results)

--- indexation/searcher.py
+++ indexation/searcher.py
@@ -12,7 +12,50 @@ from org.apache.lucene.search.similarities import \
 
 N_DOCS = 10
 
-def run(searcher, analyzer, reordering='no'):
+
+def reorder_ups(scoreDocs, searcher):
+    """Reorder results using the score 'n_docs - rank + ups'
+
+    Return (ordered_scoreDocs, score_values)"""
+    scores = {}
+    n_docs = len(scoreDocs)
+    for i, scoreDoc in enumerate(scoreDocs):
+        doc = searcher.doc(scoreDoc.doc)
+        scores[scoreDoc.doc] = (n_docs-i, int(doc.get('ups')))
+    scoreDocs = sorted(scoreDocs, key=lambda sd: -sum(scores[sd.doc]))
+    return scoreDocs, scores
+
+def reorder_normups(scoreDocs, searcher, weights=(0.8,0.2)):
+    """Reorder results using the score w[0]*sc_rank + w[1]*sc_ups
+       where sc_rank = (n_docs-rank)/n_docs
+             sc_ups = ups / max(abs(ups_of_subreddit))
+
+    Return (ordered_scoreDocs, score_values)
+    """
+    scores = {}
+    n_docs = len(scoreDocs)
+    for i, scoreDoc in enumerate(scoreDocs):
+        doc = searcher.doc(scoreDoc.doc)
+        query_ups = QueryParser("subreddit", analyzer).parse(doc.get('subreddit'))
+        scoreDocs_ups = searcher.search(query_ups, 1000).scoreDocs
+        max_ups = 0
+        for sd_ups in scoreDocs_ups:
+            max_ups = max(max_ups, abs(int(searcher.doc(sd_ups.doc).get('ups'))))
+        if max_ups == 0:
+            max_ups = 1
+
+        w_rank, w_ups = 0.7, 0.3
+        scores[scoreDoc.doc] = (w_rank * (n_docs-i)/n_docs,
+                                w_ups * int(doc.get('ups'))/max_ups)
+    scoreDocs = sorted(scoreDocs, key=lambda sd: -sum(scores[sd.doc]))
+    return scoreDocs, scores
+
+
+
+
+
+
+def run(searcher, analyzer, reordering='no', show_bodies=True):
     while True:
         print()
         print("Hit enter with no input to quit.")
@@ -27,41 +70,17 @@ def run(searcher, analyzer, reordering='no'):
         print("%s total matching documents." % len(scoreDocs))
 
 
-        # Reordering using rank and ups 
-        n_docs = len(scoreDocs)
         if reordering == 'ups':
-            scores = {}
-            for i, scoreDoc in enumerate(scoreDocs):
-                doc = searcher.doc(scoreDoc.doc)
-                scores[scoreDoc.doc] = (n_docs-i, int(doc.get('ups')))
-            scoreDocs = sorted(scoreDocs, key=lambda sd: -sum(scores[sd.doc]))
-
-        # Reordering using weighted normalized rank and ups
+            scoreDocs, scores = reorder_ups(scoreDocs, searcher)
         elif reordering == 'normups':
-            scores = {}
-            for i, scoreDoc in enumerate(scoreDocs):
-                doc = searcher.doc(scoreDoc.doc)
-                query_ups = QueryParser("subreddit", analyzer).parse(doc.get('subreddit'))
-                scoreDocs_ups = searcher.search(query_ups, 1000).scoreDocs
-                max_ups = 0
-                for sd_ups in scoreDocs_ups:
-                    max_ups = max(max_ups, abs(int(searcher.doc(sd_ups.doc).get('ups'))))
-                if max_ups == 0:
-                    max_ups = 1
-
-                w_rank, w_ups = 0.7, 0.3
-                scores[scoreDoc.doc] = (w_rank * (n_docs-i)/n_docs,
-                                        w_ups * int(doc.get('ups'))/max_ups)
-            scoreDocs = sorted(scoreDocs, key=lambda sd: -sum(scores[sd.doc]))
-
-        # No reordering
+            scoreDocs, scores = reorder_normups(scoreDocs, searcher)
         else:
+            n_docs = len(scoreDocs)
             scores = {sd.doc: (n_docs-i,) for i,sd in enumerate(scoreDocs)}
 
         # Print results
         for i, scoreDoc in enumerate(scoreDocs):
             doc = searcher.doc(scoreDoc.doc)
-            print()
             print("%d: %s (score: %s)"%(
                 i,
                 doc.get('name'),
@@ -70,7 +89,8 @@ def run(searcher, analyzer, reordering='no'):
                     sum(scores[scoreDoc.doc])
                     )
                 ))
-            print(doc.get('body'))
+            if not show_bodies:
+                print(doc.get('body'), "\n")
 
         
 
@@ -91,6 +111,8 @@ if __name__ == "__main__":
                         default="tfidf", help="Similarity (in [tfidf, lm, bm25])")
     parser.add_argument('--reorder', type=str, nargs='?',
                         default="no", help="Reordering (in [ups, normups])")
+    parser.add_argument('--short', action='store_false',
+                        help="Don't show the body of comments")
     args = parser.parse_args()
 
 
@@ -107,4 +129,4 @@ if __name__ == "__main__":
     if similarity is not None:
         searcher.setSimilarity(similarity)
     analyzer = StandardAnalyzer()
-    run(searcher, analyzer, reordering=args.reorder)
+    run(searcher, analyzer, reordering=args.reorder, show_bodies=not args.short)
