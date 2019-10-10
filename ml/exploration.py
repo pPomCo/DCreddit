ba34765245f6409494c0d463aa90a96e444c76b4
doucet julian <dcj1924a@u1203-10l.ens.fsi>
2019-10-10 15:43:53 +0200

    Affichage feature

--- ml/exploration.py
+++ ml/exploration.py
@@ -1,10 +1,13 @@
 from preprocessing import *
 from datetime import datetime
 from dateutil import tz
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
+from sklearn.metrics import mean_absolute_error
 
 #E: Les donnees en data frame
 #F: Créer la matrice de var covar pour nos données et l'afficher
@@ -12,11 +15,14 @@ import matplotlib.pyplot as plt
 def mcov(df):
 	
 	###Taille des mots
-	csv = pd.read_csv('jultxt.csv',sep='\t', names=['id','taille_body'])
-	
-	csv['taille_body'] = csv['taille_body'].apply(lambda x: len(x))
-	
-	df = df.merge(csv,on='id')
+	(a,b) = df.shape
+	print(a)
+	if a<15000:
+		csv = pd.read_csv('jultxt.csv',sep='\t', names=['id','taille_body'])
+
+		csv['taille_body'] = csv['taille_body'].apply(lambda x: len(( " ".join(eval(x)) ).strip())) 
+
+		df = df.merge(csv,on='id')
 	
 	object_columns = [
 		'subreddit_id', 
@@ -72,12 +78,46 @@ def mcov(df):
 	plt.show()
 	
 	return
+	
+#E: Le dataframe de nos donnees
+#F: Ajoute la representation vectorielle du body au data frame
+#S: Le dataframe modifié
+def bodyVectorise(df):
 
+	cols = [i for i in range(201)]
+	cols[200]='id'
+	csv = pd.read_csv('jultxtVec.csv',sep='\t',names=cols)
+	x = df.merge(csv,on='id')
+	
+	uselessFeatures =['subreddit_id', 'link_id', 'name', 
+	'author_flair_css_class', 'author_flair_text', 
+	'subreddit', 'id', 'removal_reason', 'author', 
+	'body', 'distinguished', 'parent_id']
+	
+	x.drop((uselessFeatures),axis=1,inplace=True)
+	print(x)
+
+	y = x['ups']
+	print(y)
+	x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.70, random_state=42)
+		
+	#AJOUT DE CROSS VALIDATION CAR SURAPPRENTISSAGE	
+	
+	model = XGBRegressor(random_state=42,eval_metric="mae")
+	model.fit(x_train,y_train)
+	
+	y_pred = model.predict(x_test)
+	mae = mean_absolute_error(y_test,y_pred)
+	print(mae)
+	return
+	
 def main():
-	
-	df = get_data_db("../projet reddit/sample_3days.sqlite")
-	mcov(df)
-	
+	#La premiere heure
+	df = get_data_db("../projet reddit/sample.sqlite")
+	#Les 3 premiers jours
+	#df = get_data_db("../projet reddit/sample_3days.sqlite")
+	bodyVectorise(df)
+	#mcov(df)
 	return
 
 if __name__ == '__main__':
