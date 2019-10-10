f9fa55fe72edb8ba71da5bec88863ea4d1e25c9f
doucet julian <dcj1924a@u1209-20l.ens.fsi>
2019-10-10 11:38:11 +0200

    Affichage features

--- ml/exploration.py
+++ ml/exploration.py
@@ -1,4 +1,6 @@
 from preprocessing import *
+from datetime import datetime
+from dateutil import tz
 import pandas as pd
 import seaborn as sns
 import numpy as np
@@ -9,15 +11,12 @@ import matplotlib.pyplot as plt
 #S: Rien (fonction d'affichage)
 def mcov(df):
 	
-	csv = pd.read_csv('jultxt.csv',sep='\t', names=['id','body'])
+	###Taille des mots
+	csv = pd.read_csv('jultxt.csv',sep='\t', names=['id','taille_body'])
 	
-	csv['body'] = csv['body'].apply(lambda x: len(x))
+	csv['taille_body'] = csv['taille_body'].apply(lambda x: len(x))
 	
-	df.drop(['downs','archived'],axis=1,inplace=True)
-	
-	print(df.columns, csv.columns)
-	print(df['id'])
-	df=df.merge(csv,on='id')
+	df = df.merge(csv,on='id')
 	
 	object_columns = [
 		'subreddit_id', 
@@ -32,33 +31,53 @@ def mcov(df):
 		'distinguished',
 		'parent_id',
 	]
+	
 	d = {
 		col: 'None' for col in object_columns
 	}
 	
 	df.fillna(d,inplace=True)
 	
+	###Date
+	df_heure = df[['id','created_utc']]
+	
+	df_heure['heure'] = df_heure['created_utc'].apply(lambda x:datetime.utcfromtimestamp(x).hour)
+	df_heure.drop('created_utc',  axis=1, inplace=True)
+	
+	df=df.merge(df_heure,on='id')
+	
+	###tfxidf
+	
+	###Affichage
+	
+	df.drop(['downs','archived'],axis=1,inplace=True)
+
 	corr = df.corr()
 	
 	mask = np.zeros_like(corr, dtype=np.bool)
 	mask[np.triu_indices_from(mask)] = True
 	
-	f, ax = plt.subplots(figsize=(11, 9))
+	f, ax = plt.subplots(figsize=(11, 10))
 	
 	cmap = sns.diverging_palette(220, 10, as_cmap=True)
+	
+	cmap2 = sns.cubehelix_palette(as_cmap=True)
 	
 	sns.heatmap(corr, cmap=cmap, mask=mask, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 	
-	sns.pairplot(df)
+	df['upsNormalize'] = df['ups'].apply(lambda x: len(str(x)))
+	sns.pairplot(df, diag_kind="kde", markers="+",hue='upsNormalize')
 	
 	plt.show()
 	
 	return
 
 def main():
-	df = get_data_db("../projet reddit/sample.sqlite")
+	
+	df = get_data_db("../projet reddit/sample_3days.sqlite")
 	mcov(df)
+	
 	return
 
 if __name__ == '__main__':
