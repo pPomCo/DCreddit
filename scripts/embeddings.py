73083df2ec7efe5a4bbbf7e19656687d15f2d23e
Jesus L <jeslev4@gmail.com>
2019-10-14 14:52:47 +0200

    vector and users embeddings added online

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -25,15 +25,17 @@ from cleantext import *
 
 banned_users = "set(['', '[deleted]', 'AutoModerator', 'autowikibot', 'TweetsInCommentsBot', 'TweetPoster', 'TrollaBot', 'MTGCardFetcher', 'havoc_bot', 'TotesMessenger', 'TheNitromeFan', 'PoliticBot', 'autotldr', 'Late_Night_Grumbler', 'SharksPwn', 'WritingPromptsRobot', 'atomicimploder', 'Marvelvsdc00'])"
 
-#path_db= "/home/jeslev/Downloads/sample_3days.sqlite"
+default_path_db= "/home/jeslev/Downloads/sample_3days.sqlite"
 glove_path = "/home/jeslev/masterUPS/M2/S1/Projet/glove.6B.200d.txt"
 tmp_path = "/home/jeslev/masterUPS/M2/S1/Projet/glove.6B.200d_word2vec.txt"
 fcomments_embeddings = "textVec.csv"
 fusers_embeddings = "userVec.csv"
+chunksize = 1*(10 ** 5)
 
-def connectDB(db="/home/jeslev/Downloads/sample_3days.sqlite"):
+def connectDB(db=default_path_db):
     '''
-    Connect to db file
+    Connect to db file.
+    Returns: Cursor
     '''
     conn = sqlite3.connect(db)
     return conn.cursor()
@@ -41,7 +43,11 @@ def connectDB(db="/home/jeslev/Downloads/sample_3days.sqlite"):
 
 def importEmbeddings(glove_path):
     '''
-    Create matrix and index for wikipedia glove embeddings
+    Create matrix and index for wikipedia glove embeddings from glove_path file.
+    Returns:
+    - matrix_embeddings: vector for all words of glove dictionary
+    - word2num: dictionary to identify row of a word
+    - num2word: dictionary to identify word of a row
     '''
     maxwords = 400000
     dim = 200
@@ -66,6 +72,10 @@ def importEmbeddings(glove_path):
 
 
 def getCleanedComments(conn):
+    '''
+    Returns the comment id, author, subreddit id and list of valid words for
+    each comment in the database
+    '''
     ignored_users = eval(banned_users)
     query = "SELECT id, author, subreddit_id, body from May2015"
     for line in conn.execute(query):
@@ -86,6 +96,28 @@ def getCleanedComments(conn):
         yield comment_id, author, subreddit_id, listwords
 
     
+def getCleanedCommentsInBatch(lines):
+    '''
+    Same as getCleanedComments but in batchs
+    '''
+    ignored_users = eval(banned_users)
+    for line in lines:
+        comment_id = line[0]
+        author = line[1]
+        subreddit_id = line[2]
+        text = line[3]
+        
+        # if it is a banned user we skip
+        if author in ignored_users:
+            continue
+        
+        listwords = clean_text(text)
+        # empty string
+        if len(listwords)==0:
+            continue        
+        yield comment_id, author, subreddit_id, listwords
+
+
 def getVectorEmb(list_words, model, word2num, num2word, dim=200):
     '''
     Calculate vector embedding : average term vector of comment
@@ -112,6 +144,7 @@ def getVectorEmb(list_words, model, word2num, num2word, dim=200):
 def prepareEmbeddingSet():
     '''
     Create a file with all embeddings (200d) for each comment in the dataset
+    Returns: Write a CSV file with all the vector for the database comments
     '''
     print("Connecting db...")
     db = connectDB()
@@ -135,9 +168,8 @@ def prepareEmbeddingSet():
 
 def getAllSubReddits():
     '''
-    Get all subreddits and a dictionary of id_comment -> id_subreddit
+    Returns  all subreddits and a dictionary of id_comment -> id_subreddit
     '''
-
     c = connectDB()
     subrd = set()
     mapSubs = dict()
@@ -152,14 +184,16 @@ def processComments(chunk, com2sub, le, matrix, count_matrix):
     '''
     Update matrix and count_matrix with the embedded vector and frequency
     respectively.
+    Returns: matrix and count_matrix updated
     '''
-    df = pd.DataFrame(chunk)
+    df = chunk#pd.DataFrame(chunk)
+    #print(df.head())
     cols = [i for i in range(3,203)]  # col names for df
     comm = df[0] # comment id
     comments_sub_names = comm.apply(lambda x: com2sub[x])
     sub_ids = le.transform(comments_sub_names)
     
-    np.add.at(matrix,sub_ids,df[cols])
+    np.add.at(matrix,sub_ids,df[cols].astype(np.single))
     np.add.at(count_matrix,sub_ids, 1)
     #print(matrix, np.sum(count_matrix))
     return matrix, count_matrix
@@ -167,7 +201,7 @@ def processComments(chunk, com2sub, le, matrix, count_matrix):
 
 def findUserComment(row, centre_clusters, prefs):
     vcols = [str(i) for i in range(200)]
-    print(row[vcols])
+    #print(row[vcols])
     comment = np.array(row[3:203], dtype=np.double) # embedding
     clusters = np.argsort(LA.norm(centre_clusters - comment, axis=1))[:11]
 
@@ -214,9 +248,48 @@ def prepareUserEmbeddings():
     filename = fcomments_embeddings
     vcols = [str(i) for i in range(200)]
     cols = np.append(np.array(['id','user','subr']), vcols)
-    chunksize = 5*(10 ** 5)
+    
     for chunk in pd.read_csv(filename, chunksize=chunksize ,sep='\t', header=None, names=cols):
         #print("Batch")
+        vecUsers = processUserComments(chunk, centre_clusters, vecUsers)
+    
+    print("Normalizing vectors...")
+    #normalize vector
+    with open(fusers_embeddings, mode='w') as f:
+        writer = csv.writer(f, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for key,val in vecUsers.items():
+            vectorU = val/val.max()
+            line = np.append( np.array([key]),vectorU.astype(np.float16))
+            writer.writerow(line)
+
+
+def prepareUserEmbeddingsOnline():
+    '''
+    Write userVec file with the embedding profile for all users
+    '''
+    print("Connecting db...")
+    db = connectDB()
+    print("Loading Glove embeddings...")
+    glove, word2num, num2word = importEmbeddings(glove_path)
+    
+    vecUsers = dict()
+    centre_clusters = np.load('cluster_centers.npy')
+
+    #load emb vectors
+    vcols = [str(i) for i in range(200)]
+    cols = np.append(np.array(['id','user','subr']), vcols)
+    print("Cleaning text files...")
+    query = "SELECT id, author, subreddit_id, body FROM May2015 WHERE created_utc<1432512000 LIMIT 500000"
+    db.execute(query)
+    for batch in iter(lambda: db.fetchmany(chunksize), []):
+        dfbatch = []
+    
+        for comment_id, author, subreddit_id, listwords in getCleanedCommentsInBatch(batch):
+            docVec = getVectorEmb(listwords, glove, word2num, num2word)
+            tline = np.append(np.array([comment_id, author, subreddit_id]),docVec)
+            dfbatch.append(tline)
+        print("Processing batch")
+        chunk = pd.DataFrame(dfbatch, columns = cols) 
         vecUsers = processUserComments(chunk, centre_clusters, vecUsers)
     
     print("Normalizing vectors...")
@@ -232,6 +305,7 @@ def prepareUserEmbeddings():
 def subreddit2vector():
     '''
     Return embedded vector for all subreddit in dataset
+    Returns: Save subreddit_matrix and cluster_centers variables
     '''
 
     print("Preparing subreddits info...")
@@ -247,9 +321,8 @@ def subreddit2vector():
     count_sreddit_matrix = np.zeros(totsubs)
 
     #load emb vectors
-    print("Calculatin subreddits average...")
+    print("Calculating subreddits average...")
     filename = fcomments_embeddings
-    chunksize = 5*(10 ** 5)
     for chunk in pd.read_csv(filename, chunksize=chunksize ,sep='\t', header=None):
         sreddit_matrix, count_sreddit_matrix = \
             processComments(chunk, com2sub, le, sreddit_matrix, count_sreddit_matrix)
@@ -268,15 +341,72 @@ def subreddit2vector():
 
     np.save('cluster_centers',kmeans.cluster_centers_)
 
+    
+def subreddit2vectorOnline():
+    '''
+    Return embedded vector for all subreddit in dataset from Scratch
+    Returns: Save subreddit_matrix and cluster_centers variables
+    '''
 
+    print("Connecting db...")
+    db = connectDB()
+    print("Loading Glove embeddings...")
+    glove, word2num, num2word = importEmbeddings(glove_path)
+    
+    print("Preparing subreddits info...")
+    le = preprocessing.LabelEncoder()
+    allsubs, com2sub = getAllSubReddits() # get all Subreddits
+    le.fit(list(allsubs))
+    subdict = dict() # id_sub -> vector
+
+    print("Getting comments vector")
+    # store accum vector for each subreddit
+    totsubs = len(allsubs) # total subreddits
+    sreddit_matrix = np.zeros((totsubs,200))
+    count_sreddit_matrix = np.zeros(totsubs)
+
+    #load emb vectors
+    print("Cleaning text files...")
+    query = "SELECT id, author, subreddit_id, body FROM May2015 WHERE created_utc<1432512000 LIMIT 500000"
+    db.execute(query)
+    for batch in iter(lambda: db.fetchmany(chunksize), []):
+        dfbatch = []
+    
+        for comment_id, author, subreddit_id, listwords in getCleanedCommentsInBatch(batch):
+            docVec = getVectorEmb(listwords, glove, word2num, num2word)
+            tline = np.append(np.array([comment_id, author, subreddit_id]),docVec)
+            dfbatch.append(tline)
+        print("Processing batch")
+        chunk = pd.DataFrame(dfbatch)
+        
+        sreddit_matrix, count_sreddit_matrix = \
+            processComments(chunk, com2sub, le, sreddit_matrix, count_sreddit_matrix)
+
+    print("Ending subreddit vectors...")
+    # Get average of comments in subreddit
+    avg_matrix = np.divide(sreddit_matrix, count_sreddit_matrix[:,None], where=count_sreddit_matrix[:,None]!=0)
+    # Save it!
+    np.save('subreddit_matrix',avg_matrix)
+
+    #bsize = (10**5)
+    # Find cluster centers
+    #kmeans = MiniBatchKMeans(n_clusters=500, random_state=42, batch_size=bsize)
+    kmeans = KMeans(n_clusters=500, random_state=42)
+    kmeans = kmeans.fit(avg_matrix)
+    np.save('cluster_centers',kmeans.cluster_centers_)
+    
+    
+    
 def main(argv):    
     if args.comments:
         #prepareEmbeddingSet(args.db)
         prepareEmbeddingSet()
     if args.subreddits:
-        subreddit2vector()
+        #subreddit2vector()
+        subreddit2vectorOnline()
     if args.users:
-        prepareUserEmbeddings()
+        #prepareUserEmbeddings()
+        prepareUserEmbeddingsOnline()
     
 
 if __name__ == "__main__":
