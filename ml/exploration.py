367f86d5f551fee89857f174c165ab5f1553f7f1
doucet julian <dcj1924a@u1208-07l.ens.fsi>
2019-10-14 11:30:15 +0200

    wordembedingGlove

--- ml/exploration.py
+++ ml/exploration.py
@@ -8,21 +8,47 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as pl
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import KFold
+
+#E: Les donnees en data frame, Un data frame comprenant l'id et le tableau du body 
+#F: Ajoute la feature de la taille du body
+#S: Le dataframe modifié
+def addTailleBody(df,csv):
+	csv = pd.read_csv(csv,sep='\t', names=['id','taille_body'])
+	
+	csv['taille_body'] = csv['taille_body'].apply(lambda x: len(( " ".join(eval(x)) ).strip())) 
+	
+	df = df.merge(csv,on='id')
+	
+	return df
+
+#E: Les donnees en data frame 
+#F: Ajoute la feature de l'heure (plus interessant que UTC)
+#S: Le dataframe modifié
+def addHour(df):
+	df_heure = df[['id','created_utc']]
+	
+	df_heure['heure'] = df_heure['created_utc'].apply(lambda x:datetime.utcfromtimestamp(x).hour)
+	df_heure.drop('created_utc',  axis=1, inplace=True)
+	
+	df=df.merge(df_heure,on='id')
+	
+	return df
+
+#E: Les donnees en data frame, les embeding des donnees 
+#F: Ajoute la feature des word embedding (glove)
+#S: Le dataframe modifié
+def addWordEmbeding(df,csv):
+	cols = [i for i in range(201)]
+	cols[200]='id'
+	csv = pd.read_csv(csv,sep='\t',names=cols)
+	df = df.merge(csv,on='id')
+	return df
 
 #E: Les donnees en data frame
-#F: Créer la matrice de var covar pour nos données et l'afficher
+#F: Créer la matrice de var covar pour nos données et l'afficher avec des nouvelle features
 #S: Rien (fonction d'affichage)
 def mcov(df):
-	
-	###Taille des mots
-	(a,b) = df.shape
-	print(a)
-	if a<15000:
-		csv = pd.read_csv('jultxt.csv',sep='\t', names=['id','taille_body'])
-
-		csv['taille_body'] = csv['taille_body'].apply(lambda x: len(( " ".join(eval(x)) ).strip())) 
-
-		df = df.merge(csv,on='id')
 	
 	object_columns = [
 		'subreddit_id', 
@@ -44,16 +70,6 @@ def mcov(df):
 	
 	df.fillna(d,inplace=True)
 	
-	###Date
-	df_heure = df[['id','created_utc']]
-	
-	df_heure['heure'] = df_heure['created_utc'].apply(lambda x:datetime.utcfromtimestamp(x).hour)
-	df_heure.drop('created_utc',  axis=1, inplace=True)
-	
-	df=df.merge(df_heure,on='id')
-	
-	###tfxidf
-	
 	###Affichage
 	
 	df.drop(['downs','archived'],axis=1,inplace=True)
@@ -63,7 +79,7 @@ def mcov(df):
 	mask = np.zeros_like(corr, dtype=np.bool)
 	mask[np.triu_indices_from(mask)] = True
 	
-	f, ax = plt.subplots(figsize=(11, 10))
+	f, ax = pl.subplots(figsize=(11, 10))
 	
 	cmap = sns.diverging_palette(220, 10, as_cmap=True)
 	
@@ -72,51 +88,76 @@ def mcov(df):
 	sns.heatmap(corr, cmap=cmap, mask=mask, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 	
-	df['upsNormalize'] = df['ups'].apply(lambda x: len(str(x)))
+	df['upsNormalize'] = df['ups'].apply(lambda x: len(str(abs(x))))
+	
 	sns.pairplot(df, diag_kind="kde", markers="+",hue='upsNormalize')
 	
-	plt.show()
+	pl.show()
 	
 	return
 	
 #E: Le dataframe de nos donnees
-#F: Ajoute la representation vectorielle du body au data frame
+#F: Affiche la mae pour un xgboost regressor des données avec le word embeding
 #S: Le dataframe modifié
 def bodyVectorise(df):
-
-	cols = [i for i in range(201)]
-	cols[200]='id'
-	csv = pd.read_csv('jultxtVec.csv',sep='\t',names=cols)
-	x = df.merge(csv,on='id')
 	
 	uselessFeatures =['subreddit_id', 'link_id', 'name', 
 	'author_flair_css_class', 'author_flair_text', 
 	'subreddit', 'id', 'removal_reason', 'author', 
-	'body', 'distinguished', 'parent_id']
+	'body', 'distinguished', 'parent_id','score_hidden',
+	'downs','score']
 	
 	x.drop((uselessFeatures),axis=1,inplace=True)
-	print(x)
-
-	y = x['ups']
-	print(y)
-	x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.70, random_state=42)
 		
-	#AJOUT DE CROSS VALIDATION CAR SURAPPRENTISSAGE	
+	#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+	kf = KFold(n_splits=2, random_state=42)
 	
-	model = XGBRegressor(random_state=42,eval_metric="mae")
-	model.fit(x_train,y_train)
+	errors=[]
 	
-	y_pred = model.predict(x_test)
-	mae = mean_absolute_error(y_test,y_pred)
-	print(mae)
+	for train_index, test_index in kf.split(x):
+		x_train =  x.iloc[train_index]
+		x_test = x.iloc[test_index]
+		
+		y_train = x_train['ups']
+		y_test = x_test['ups']
+		
+		model = XGBRegressor(random_state=42,eval_metric="mae")
+		model.fit(x_train,y_train)
+	
+		y_pred = model.predict(x_test)
+	
+		pl.scatter(y_test, y_pred,marker='+')
+		pl.xlabel("True Values")
+		pl.ylabel("Predictions")
+		pl.show()
+	
+		mae = mean_absolute_error(y_test,y_pred)
+		errors.append(mae)
+	
+	print("Erreur moyenne : ",np.mean(errors))
+
 	return
 	
 def main():
+	#Csv de la premiere heure
+	csv = 'jultxt.csv'
+	csv2 = 'jultxtVec.csv'
 	#La premiere heure
 	df = get_data_db("../projet reddit/sample.sqlite")
 	#Les 3 premiers jours
 	#df = get_data_db("../projet reddit/sample_3days.sqlite")
-	bodyVectorise(df)
+	
+	df = addTailleBody(df,csv)
+	
+	df = addHour(df)
+	
+	df = addWordEmbeding(df,csv2)
+	
+	print(df)
+	
+	#bodyVectorise(df)
+	
 	#mcov(df)
 	return
 
